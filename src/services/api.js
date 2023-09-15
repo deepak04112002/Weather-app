fa02dcab480e46328ef9b081a81b54808f20cbce
Your Name <you@example.com>
2023-09-15 22:45:27 +0530

    api key changed

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '8f9009e0c1e0ee17438bbff77df3f0ae';
+const API_KEY = '8d2a110b6ad468ae1a0e459757cf659d';
 const API_URL = 'http://api.openweathermap.org/data/2.5/weather';
 
 export const getWeather = async (city, country) => {
